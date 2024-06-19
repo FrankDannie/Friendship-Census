7d781413bbf044ced31fa0d3b20d016e1556850d
FrankDannie <frank.dannie@gmail.com>
2024-06-19 18:08:51 +0900

    Fix age by data

--- src/services/resasService.js
+++ src/services/resasService.js
@@ -1,17 +1,17 @@
+// src/services/resasService.js
 import axios from 'axios';
 
 const API_KEY = 'uhVxcOI4BDuCuE7RKLrl1eumVL89Q93LtFy495Zz'; // Replace with your actual API key
 const BASE_URL = 'https://opendata.resas-portal.go.jp/api/v1';
 
-export const fetchPopulationData = async () => {
+export const fetchPopulationDataByPrefecture = async (prefCode) => {
   try {
     const response = await axios.get(`${BASE_URL}/population/composition/perYear`, {
       headers: { 'X-API-KEY': API_KEY },
-      params: { prefCode: 1, cityCode: '-' }
+      params: { prefCode, cityCode: '-' }
     });
-    console.log(response); // Log the response to check its structure
     if (response.data && response.data.result && response.data.result.data) {
-      return response.data.result.data[0].data;
+      return response.data.result.data;
     } else {
       throw new Error('Unexpected API response structure');
     }
@@ -20,6 +20,7 @@ export const fetchPopulationData = async () => {
     throw error;
   }
 };
+
 export const fetchPrefectures = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/prefectures`, {
